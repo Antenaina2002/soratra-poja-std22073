0c7fad96a5817303ccf83733e0064b05b61e606d
Antenaina2002 <hei.antenaina@gmail.com>
2024-03-25 09:25:56 +0300

    feat: added model Soratra

---
+++ src/main/java/hei/school/soratra/repository/model/Soratra.java
@@ -0,0 +1,22 @@
+package hei.school.soratra.repository.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Table(name = "soratra")
+@Entity
+public class Soratra {
+    @Id
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "name")
+    private String name;
+}
